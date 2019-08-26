16033a63609ce78182623c1a6074b4e31b10d07b
Gijs Keij <gijs@keij.org>
2019-08-26 16:25:46 +0200

    changed to gmkey

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ if [[ $EUID -ne 0 ]]; then
 	else
     	mkdir Pimusic
     	cd Pimusic
-    	wget https://raw.githubusercontent.com/AlexProgrammerDE/Pimusic/master/install-services.sh
+    	wget https://raw.githubusercontent.com/gmkey/Pimusic/master/install-services.sh
     	sudo bash install-services.sh | tee -a install-output.txt
 	rm install-services.sh
-	wget https://raw.githubusercontent.com/AlexProgrammerDE/Pimusic/master/update-services.sh
+	wget https://raw.githubusercontent.com/gmkey/Pimusic/master/update-services.sh
     	cd ..
     	rm install.sh
 fi
